38ec4f46b4c272c46a4d9fd9d0e0f83179b0d7ad
Kelvin Ziro <kelvin.ziroh@gmail.com>
2020-10-19 00:06:46 +0300

    Add Header section to chat page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chats</title>
+</head>
+<body>
+    <header>
+        <input type="text" placeholder="Search">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Chats</a></li>
+            <li><a href="">About Us</a></li>
+        </ul>
+        <img src="" alt="User Profile">
+    </header>
+</body>
+</html>
