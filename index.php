35a9b42e062c3c37eec7fe810449c952e62a7e06
Kelvin Ziro <kelvin.ziroh@gmail.com>
2020-10-19 08:53:56 +0300

    add Topics section

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 </head>
 <body>
     <header>
+        <i class="fa fa-search" aria-hidden="true"></i>
         <input type="text" placeholder="Search">
         <ul>
             <li><a href="">Home</a></li>
@@ -18,7 +19,7 @@
     </header>
     <main>
         <div class="side-nav-bar">
-            <li><i class="fa fa-pencil" aria-hidden="true"></i></li>
+            <li><i class="fa fa-pencil" aria-hidden="true">Create New</i></li>
             <li><i class="fa fa-signal" aria-hidden="true">Dashboard</i></li>
             <li><i class="fa fa-user" aria-hidden="true">Your Profile</i></li>
             <li><i class="fa fa-comments" aria-hidden="true">Chat</i></li>
@@ -27,8 +28,18 @@
             <li><i class="fa fa-sign-out" aria-hidden="true">Logout</i></li>
         </div>
         <div class="Topics-bar">
+            <h3>Topics</h3>
         </div>
         <div class="chat-view">
+            <ul>
+                <li>All messages</li>
+                <li>Archive</li>
+                <li>Deleted</li>
+                <div class="text-area">
+                    <input type="text" placeholder="Write a message">
+                    <input type="button" value="send">
+                </div>    
+            </ul>
         </div>
     </main>
 </body>
