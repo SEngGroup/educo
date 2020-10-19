ebeaf938a9f4a93f805b321a28a5798b52070c1b
Kelvin Ziro <kelvin.ziroh@gmail.com>
2020-10-19 08:52:03 +0300

    Add side navigation and chat box

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Chats</title>
 </head>
 <body>
@@ -17,6 +18,13 @@
     </header>
     <main>
         <div class="side-nav-bar">
+            <li><i class="fa fa-pencil" aria-hidden="true"></i></li>
+            <li><i class="fa fa-signal" aria-hidden="true">Dashboard</i></li>
+            <li><i class="fa fa-user" aria-hidden="true">Your Profile</i></li>
+            <li><i class="fa fa-comments" aria-hidden="true">Chat</i></li>
+            <li><i class="fa fa-globe" aria-hidden="true">Help Center</i></li>
+            <li><i class="fa fa-cog" aria-hidden="true">Settings</i></li>
+            <li><i class="fa fa-sign-out" aria-hidden="true">Logout</i></li>
         </div>
         <div class="Topics-bar">
         </div>
