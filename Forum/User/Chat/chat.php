b1c11f9404e43dfc9b5a4c5065f48c1e9c72631d
Rees5 <reesalumasa@gmail.com>
2020-11-13 00:02:28 +0300

    update chat directory

--- Forum/User/Chat/chat.php
+++ Forum/User/Chat/chat.php
@@ -1,20 +1,20 @@
-<?php include 'header.php'; ?>
+<?php include '../header.php'; ?>
     <div class="container">
         <div class="side-nav-bar">
             <ul class="chat-side-nav">
-              <a href="new_topic.php" id="create-user"><li id="create-chat"><i class="fa fa-pencil side-nav" aria-hidden="true" ></i>Create New</li></a>
+              <a href="../new_topic.php" id="create-user"><li id="create-chat"><i class="fa fa-pencil side-nav" aria-hidden="true" ></i>Create New</li></a>
 
-                <li class="side-links"><a href="dashboard.php"><i class="fa fa-signal side-nav" aria-hidden="true"></i>Dashboard</a></li>
-                <li class="side-links"><a href="profile.php"><i class="fa fa-user side-nav" aria-hidden="true"></i>Your Profile</a></li>
-                <li class="side-links"><a href="forum.php"><i class="fa fa-users side-nav" aria-hidden="true"></i>Forum</a></li>
+                <li class="side-links"><a href="../dashboard.php"><i class="fa fa-signal side-nav" aria-hidden="true"></i>Dashboard</a></li>
+                <li class="side-links"><a href="../profile.php"><i class="fa fa-user side-nav" aria-hidden="true"></i>Your Profile</a></li>
+                <li class="side-links"><a href="../forum.php"><i class="fa fa-users side-nav" aria-hidden="true"></i>Forum</a></li>
                 <li class="side-links" style="background-color: rgba(0, 255, 255, 0.2);"><a href="" style="color: #00ffff;"><i class="fa fa-comments side-nav" aria-hidden="true"></i>Chat</a></li>
                 <li class="side-links"><a href=""><i class="fa fa-globe side-nav" aria-hidden="true"></i>Help Center</a></li>
                 <li class="side-links cog"><a href=""><i class="fa fa-cog side-nav" aria-hidden="true"></i>Settings</a></li>
-                <li class="side-links"><a href="../../src/auth/test_auth.php?logout='1"><i class="fa fa-sign-out side-nav" aria-hidden="true"></i>Logout</a></li>
+                <li class="side-links"><a href="../../../src/auth/test_auth.php?logout='1"><i class="fa fa-sign-out side-nav" aria-hidden="true"></i>Logout</a></li>
             </ul>
         </div>
         <div class="main" style="width: 83%;">
-          <?php include 'top_nav.php'; ?>
+          <?php include '../top_nav.php'; ?>
 
             <div class="chat-area" style="height: 636px;">
                 <div class="topics-bar">
@@ -42,7 +42,7 @@
             </div>
 </div>
     </div>
-    <?php include 'newpost.php'; ?>
+    <?php include '../newpost.php'; ?>
     </div>
 </body>
 </html>
--- Forum/User/header.php
+++ Forum/User/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../../assets/css/style.css">
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
