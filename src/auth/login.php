535425291061fd3acd5904fe24443abd6aaed0b8
Rees5 <reesalumasa@gmail.com>
2020-11-20 21:12:47 +0300

    fix google auth bug

--- src/auth/login.php
+++ src/auth/login.php
@@ -50,6 +50,10 @@ if (isset($_SESSION['user_id'])) {
         // The ID token you need to pass to your backend:
         var id_token = googleUser.getAuthResponse().id_token;
         console.log("ID Token: " + id_token);
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+          console.log('User signed out.');
+        });
       }
     </script>
     <style media="screen">
