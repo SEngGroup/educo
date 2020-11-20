e0ad205ef97646f9da28fa1b7321f75d487ac422
Rees5 <reesalumasa@gmail.com>
2020-11-20 20:10:50 +0300

    Add signin with google functionality

--- src/auth/login.php
+++ src/auth/login.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-signin-scope" content="profile email">
+    <meta name="google-signin-client_id" content="615106965069-55mjfn0j0eoqfli2nrbiipfp2l8a8eka.apps.googleusercontent.com">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
     <title>Login</title>
     <link rel="stylesheet" href="styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
@@ -20,15 +23,41 @@
             <input type="password" id="password" name="pwd" placeholder="Enter your password" required>
             <p style="font-size: 12px; color: gray; position: relative; left: 240px; bottom: 20px;">Forgot password?</p>
             <input id="button" name="login" type="submit" value="LOGIN">
-            <p style="text-align: center; color: gray;">Or Sign in using</p>
-            <ul style="display: flex; flex-direction: row; margin-left: 30px; margin-right: 70px; margin-bottom: 50px;">
-                <li><i class="fa fa-facebook" aria-hidden="true"></i></li>
-                <li><i class="fa fa-twitter" aria-hidden="true"></i></li>
-                <li><i class="fa fa-google" aria-hidden="true"></i></li>
-            </ul>
-            <p style="text-align: center; color: gray;">Don't have an account?</p>
+            <p style="text-align: center; color: gray;">Or</p>
+            <div class="well">
+              <center><div class="g-signin2" data-onsuccess="onSignIn" data-width="240" data-height="50" data-longtitle="true"></div></center>
+              <br><br>
+            </div>
+            <p style="text-align: center; color: black;">Don't have an account yet?</p>
             <p style="text-align: center;"><a href="signup.php" style="text-decoration: none; color: gray; font-size: 18px;">Sign Up</a></p>
         </form>
     </div>
+    <script>
+      function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      }
+    </script>
+    <style media="screen">
+
+.well {
+text-align : center;
+}
+a:link {
+  text-decoration : none;
+  font-family : Station Medium;
+  font-size:24px;
+}
+    </style>
 </body>
 </html>
