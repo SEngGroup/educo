d3d60e9f890b223e5a5264fa406de81f2179fa37
Rees5 <reesalumasa@gmail.com>
2020-11-23 12:38:14 +0300

    Update landing page/about us

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 		<title>Educo | Home</title>
         <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+        <link rel="shortcut icon" href="http://educo.epizy.com/educo/assets/img/favicon.ico" />
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link href="https://fonts.googleapis.com/css2?family=Suez+One&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
